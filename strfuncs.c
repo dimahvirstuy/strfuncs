459d8b8a1b72c54059ad62d57fae0785a0aaf3c7
ashneeldas2 <adas2@stuy.edu>
2017-10-05 11:34:35 -0400

    fixed formatting and stuff

--- strfuncs.c
+++ strfuncs.c
@@ -5,11 +5,11 @@
 int my_strcmp (char *a, char *b) {
   int i = 0;
   while (a[i]) {
-    if (a[i] > b[i]) return -1;
-    if (b[i] > a[i]) return 1;
+    if (b[i] > a[i]) return a[i] - b[i];
+    if (a[i] > b[i]) return a[i] - b[i];
     i++;
   }
-  if (b[i]) return 1;
+  if (a[i]) return a[i];
   return 0;
 }
 
@@ -51,6 +51,7 @@ char * my_strncpy(char * dest, char * source, int n) {
 }
 
 int main() {
+  char zero[100] = "hi";
   char one[] = "Obama";
   char two[] = "Trump";
   char three[] = "Lincoln";
@@ -64,20 +65,32 @@ int main() {
   printf("%s compared to %s is: %d\n",one, six, my_strcmp(one, six));
   printf("%s compared to %s is: %d\n",five, two, my_strcmp(five, two));
   printf("%s compared to %s is: %d\n",five, six, my_strcmp(five, six));
-  printf("%s compared to %s is: %d\n",one, one, my_strcmp(one, one));
-  printf("\nstrlen testing:\n\n");//strlen testing
+  printf("%s compared to %s is: %d\n\n",one, one, my_strcmp(one, one));
+
+  printf("strcmp actual results:\n\n");
+  printf("%s compared to %s is: %d\n", one, two, strcmp(one, two));
+  printf("%s compared to %s is: %d\n",three, four, strcmp(three, four));
+  printf("%s compared to %s is: %d\n",one, six, strcmp(one, six));
+  printf("%s compared to %s is: %d\n",five, two, strcmp(five, two));
+  printf("%s compared to %s is: %d\n",five, six, strcmp(five, six));
+  printf("%s compared to %s is: %d\n\n",one, one, strcmp(one, one));
+
+  printf("My strlen:\n");
   printf("%s is %d characters long\n",three, my_strlen(three));
   printf("%s is %d characters long\n",six, my_strlen(six));
-  printf("%s is %d characters long\n",one, my_strlen(one));
-  printf("\nstrcat testing:\n\n");//strcat testing 
-  printf("%s concatenated to %s is: %s\n",one, three, my_strcat(one, three));
-  printf("%s concatenated to %s is: %s\n",two, five, my_strcat(two, five));
-  printf("%s concatenated to %s is: %s\n",three, six, my_strcat(three, six)); 
-  printf("\nstrchr testing:\n\n");
-  printf("%s\n", one);
-  printf("The substring after character %c in string %s is %s\n", c, one, my_strchr(one, c)); // should be ama
-  printf("THe substring after character %c in string %s is %s\n", c, four, my_strchr(four, c)); // should be null
-  printf("\nstrcpy testing:\n\n");
-  printf("After copying %d characters of %s to %s, the result is: %s\n", 5, two, one, my_strncpy(one, two, 5)); // should be Trump
-  printf("After copying %d characters of %s to %s, the result is: %s\n", 5, three, one, strncpy(one, three, 5)); // should be Linco
+  printf("%s is %d characters long\n\n",one, my_strlen(one));
+
+  printf("Actual strlen:\n");
+  printf("%s is %lu characters long\n",three, strlen(three));
+  printf("%s is %lu characters long\n",six, strlen(six));
+  printf("%s is %lu characters long\n",one, strlen(one));
+
+  printf("\nstrcat is [%s]\n", strcat(zero, three));
+  printf("my_strcat is [%s]\n", my_strcat(zero, three));
+
+  printf("\nmy_strchr is [%s]\n", my_strchr(one, c)); // should be ama
+  printf("strchr is [%s]\n", strchr(one, c)); // should be ama
+
+  printf("\nmy strn_cpy: [%s]\n", my_strncpy(one, two, 5)); // should be Trump
+  printf("strn_cpy: [%s]\n", strncpy(one, two, 5)); // should be Trump
 }
